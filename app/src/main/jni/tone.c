b432bb337181b33f1e0d6682379a62bf9eec50b0
Don Turner <don@chonk.co.uk>
2015-07-06 14:45:23 +0100

    Initial commit

---
+++ app/src/main/jni/tone.c
@@ -0,0 +1,11 @@
+#include <jni.h>
+
+void Java_com_example_audio_generatetone_MainActivity_playTone(JNIEnv* env, jclass clazz){
+
+    int count = 0;
+    while (count < 10){
+        count++;
+    }
+
+}
+
